b6f57aee3e6cf246f14b8671ce8b1092fe75b2ae
Nicole Houston <nicole.houston@cox.net>
2018-05-25 14:04:11 -0700

    append info from year to table

--- javascript.js
+++ javascript.js
@@ -1,28 +1,31 @@
 
-// Initialize and add the map
-function initMap(array) {
-    // The location of Uluru
-    var uluru = {lat: array[0].geolocation.coordinates[0], lng: array[0].geolocation.coordinates[1]};
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 2, center: uluru});
-    // The marker, positioned at Uluru
-    for(var i = 0; i < array.length; i++){
-        var coordinates = {lat: array[i].geolocation.coordinates[0], lng: array[i].geolocation.coordinates[1]};
-        var marker = new google.maps.Marker({position: coordinates, map: map});
-    }
-  }
+// // Initialize and add the map
+// function initMap(array) {
+//     // The location of Uluru
+//     var uluru = {lat: array[0].geolocation.coordinates[0], lng: array[0].geolocation.coordinates[1]};
+//     // The map, centered at Uluru
+//     var map = new google.maps.Map(
+//         document.getElementById('map'), {zoom: 2, center: uluru});
+//     // The marker, positioned at Uluru
+//     for(var i = 0; i < array.length; i++){
+//         var coordinates = {lat: array[i].geolocation.coordinates[0], lng: array[i].geolocation.coordinates[1]};
+//         var marker = new google.maps.Marker({position: coordinates, map: map});
+//     }
+//   }
 
     $.ajax({
         url: "https://data.nasa.gov/resource/y77d-th95.json?year=1950-01-01T00:00:00.000",
         method: "GET"
     }).then(function(response){
         console.log(response);
+        var newRow = $("<tr>")
+        var name = $("<td>").append(response[0].name);
+        var mass = $("<td>").append(response[0].mass);
+        var year = $("<td>").append(response[0].year);
+        var location = $("<td>").append(response[0].geolocation.coordinates);
 
-        var locations = response;
-        initMap(locations);
+        $(".meteorTable").append(newRow.append(name).append(mass).append(year).append(location));
 
-       
     })
 
 
