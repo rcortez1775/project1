ce842e13251f902dd21c73e5e022eb50ea74e82d
Paul hartley <Paulmhartley@yahoo.com>
2018-05-25 14:20:35 -0700

    a change to incorporate the input field input to add it to the query in the api

--- javascript.js
+++ javascript.js
@@ -1,4 +1,11 @@
 
+// Grab text the user typed into the search input,
+yearChosen = $("#InputYear").val().trim();
+
+
+console.log(yearChosen)
+
+
 // Initialize and add the map
 function initMap(array) {
     // The location of Uluru
@@ -20,7 +27,7 @@ function initMap(array) {
 
         var locations =[];
         for (var i = 0; i < response.length; i++){
-            if (response[i].year === "1951-01-01T00:00:00.000"){
+            if (response[i].year === yearChosen + "-01-01T00:00:00.000"){
                 locations.push(response[i]);
 
             }
